49aafbb14b7a7bf31201d14d2d1197518336ffb3
nehaasokan <74449428+nehaasokan@users.noreply.github.com>
2021-01-30 12:24:42 +1300

    Add files via upload
    Complete file

--- src/test/java/pagemodel/LoginPage.java
+++ src/test/java/pagemodel/LoginPage.java
@@ -9,6 +9,25 @@ WebDriver driver;
 
 By txtUserName=By.name("username");
 By txtPassWord=By.name("password");
-By btnLogin=By.linkText("linkText");
-	
+By btnLogin=By.xpath("//input[@type='submit']");
+
+public LoginPage(WebDriver driver)
+{
+	this.driver=driver;
+}
+
+
+public void enterUserName()
+{
+	driver.findElement(txtUserName).sendKeys("manty");
+}
+public void enterPassword()
+{
+	driver.findElement(txtPassWord).sendKeys("manty1");
+}
+public void clickLogin()
+{
+	driver.findElement(btnLogin).click();
+}
+
 }
